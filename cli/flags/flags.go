7bfa13644cf4bc1b972e10f2be0f283f81fd9eb3
Jorge Henriquez <contact@jorgehenriquez.dev>
2020-07-06 16:50:12 -0700

    Update main cli to support yaml input

--- cli/flags/flags.go
+++ cli/flags/flags.go
@@ -2,54 +2,11 @@ package flags
 
 import (
 	"flag"
-	"fmt"
-	"strings"
-
-	"github.com/SheetJS/rooster/filter"
 )
 
-var supportedVCS = map[string]struct{}{
-	"git": {},
-	"svn": {},
-	"hg":  {},
-}
-
-// Set represents the command line
-// flags.
-type Set struct {
-	RepoURL    string
-	VCS        string
-	OutputDir  string
-	Extensions []string
-}
-
 // Get grabs the command line flags.
-func Get() (*Set, error) {
-	pRepo := flag.String("repo", "", "The repo to grab")
-	pExt := flag.String("ext", "", "The extension(s) to filter in a comma seperated list")
-	pVCS := flag.String("type", "git", "The VCS to use [supported options: git,svn,hg]")
-	pOut := flag.String("out", "rooster_output", "The directory to store the output")
-
+func Get() (configFile string) {
+	flag.StringVar(&configFile, "config", ".rooster.yaml", "Rooster's config file")
 	flag.Parse()
-
-	vcs := strings.TrimSpace(strings.ToLower(*pVCS))
-	if _, in := supportedVCS[vcs]; !in {
-		return nil, fmt.Errorf("unsupported vsc: %s", vcs)
-	}
-
-	if *pExt == "" {
-		return nil, fmt.Errorf("missing required extension flag")
-	}
-
-	extensions, err := filter.ExtensionsFromString(*pExt)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Set{
-		RepoURL:    *pRepo,
-		VCS:        vcs,
-		Extensions: extensions,
-		OutputDir:  *pOut,
-	}, nil
+	return configFile
 }
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/SheetJS/rooster/cli/flags"
 	"github.com/SheetJS/rooster/filter"
+	"github.com/SheetJS/rooster/roosterconf"
 	"github.com/SheetJS/rooster/vcs"
 	"github.com/SheetJS/rooster/vcs/git"
 	"github.com/SheetJS/rooster/vcs/hg"
@@ -19,7 +22,7 @@ var funcMap = map[string]vcs.CloneFunc{
 	"hg":  hg.Clone,
 }
 
-func main() {
+func cloneRepoAndWrite(r roosterconf.Repository) error {
 
 	// Make a temp directory to store the cloned repo.
 	tDir, err := ioutil.TempDir(os.TempDir(), "rooster-temp-")
@@ -29,36 +32,64 @@ func main() {
 	// And make sure we cleanup after ourselves.
 	defer func() {
 		if err := os.RemoveAll(tDir); err != nil {
-			log.Fatalln(err)
+			log.Printf("failed to cleanup after repo: %s @ %s", r.RepoURL, tDir)
 		}
 	}()
 
-	// Grab the CLI flags.
-	flagSet, err := flags.Get()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	// Get the associated vsc cloning function.
-	clone, found := funcMap[flagSet.VCS]
+	clone, found := funcMap[r.VCS]
 	if !found {
-		log.Fatalf("unsupported vsc: %s\n", flagSet.VCS)
+		return fmt.Errorf("unsupported vsc: %s", r.VCS)
 	}
 
 	// Then clone the repo
-	if err := clone(flagSet.RepoURL, tDir); err != nil {
-		log.Fatalln(err)
+	if err := clone(r.RepoURL, tDir); err != nil {
+		return fmt.Errorf("failed to clone repo %s: %v", r.RepoURL, err)
 	}
 
 	// Filter out the files.
-	m, err := filter.Find(tDir, flagSet.Extensions)
+	m, err := filter.Find(tDir, r.Extensions)
+	if err != nil {
+		return fmt.Errorf("failed to filter files for repo %s: %s", r.RepoURL, err)
+	}
+
+	// Write them back to disk.
+	if err := m.WriteToPath(r.OutputDir, tDir); err != nil {
+		return fmt.Errorf("failed to write files from %s: %v", r.RepoURL, err)
+	}
+
+	return nil
+}
+
+func main() {
+
+	// Grab the flags.
+	configFile := flags.Get()
+	configFD, err := os.Open(configFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer configFD.Close()
+
+	// Parse the YAML.
+	tRepos, err := roosterconf.New(configFD)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// Write them back to disk.
-	if err := m.WriteToPath(flagSet.OutputDir, tDir); err != nil {
-		log.Fatalln(err)
+	// Grab each repo concurrently.
+	wg := new(sync.WaitGroup)
+	wg.Add(len(tRepos))
+
+	for _, repo := range tRepos {
+		go func(r roosterconf.Repository, pWG *sync.WaitGroup) {
+			defer wg.Done()
+			if err := cloneRepoAndWrite(r); err != nil {
+				log.Println(err)
+			}
+		}(repo, wg)
 	}
 
+	// Wait for all the repositories to finish downloading
+	wg.Wait()
 }
