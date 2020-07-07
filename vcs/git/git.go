fe6e85146121df4cd2ba2a8140d44be8cb7e34f2
Jorge Henriquez <contact@jorgehenriquez.dev>
2020-07-07 10:49:06 -0700

    Don't chdir when cloning

--- vcs/git/git.go
+++ vcs/git/git.go
@@ -9,14 +9,7 @@ import (
 // Clone effectively runs `git clone repoURL`.
 // The cloned repo is stored at the path.
 func Clone(repoURL, path string) error {
-	// Save the cwd.
-	cwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	defer os.Chdir(cwd)
-
-	_, err = libgit.PlainClone(path, false, &libgit.CloneOptions{
+	_, err := libgit.PlainClone(path, false, &libgit.CloneOptions{
 		URL:      repoURL,
 		Progress: os.Stderr,
 	})
--- vcs/hg/hg.go
+++ vcs/hg/hg.go
@@ -8,20 +8,8 @@ import (
 // Clone makes a child process and runs `git clone repoURL`.
 // The cloned repo is stored at the path.
 func Clone(repoURL, path string) error {
-	// Save the cwd.
-	cwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	defer os.Chdir(cwd)
-
-	// cd into the path.
-	if err := os.Chdir(path); err != nil {
-		return err
-	}
-
 	// Setup the command.
-	cmd := exec.Command("hg", "clone", repoURL)
+	cmd := exec.Command("hg", "clone", repoURL, path)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 
